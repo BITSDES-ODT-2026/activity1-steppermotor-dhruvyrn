c5d61cf145151dec5f3218b535e5a94be8d13b66
dhruvyrn <dhruv.yadav.office@gmail.com>
2026-02-18 14:18:43 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -1,1 +1,21 @@
+from machine import Pin
+import time
+
+inr1 = Pin(5, Pin.OUT)
+inr2 = Pin(4, Pin.OUT)
+inr3 = Pin(27, Pin.OUT)
+inr4 = Pin(12, Pin.OUT)
+
+pos = [[1,0,0,0],
+       [0,1,0,0],
+       [0,0,1,0],
+       [0,0,0,1]]
+
+while True:
+    for i in pos:
+        inr1.value(i[0])
+        inr2.value(i[1])
+        inr3.value(i[2])
+        inr4.value(i[3])
+        time.sleep(0.005)
 
